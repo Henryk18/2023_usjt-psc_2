2c3d8b009d7832e04cf6dababa48d85a401ea978
henrykbagda@gmail.com <823135401@ulife.com.br>
2023-09-18 21:41:37 -0300

    ap05(repeticao): calculo de media

---
+++ ExemplosBasicos.java
@@ -0,0 +1,25 @@
+public class ExemplosBasicos{
+   public static void main(String args []){
+ int cont = 1;
+ while(cont <= 3){
+   System.out.print(cont);
+cont = cont + 1;
+ }
+
+
+
+     //for(int cont = 1; <= 3;cont = cont +1){
+       // System.out.print(cont)
+    // }
+   
+   int cont = 1;
+   do{
+    System.out.printf("Valor da vez ", cont);
+    cont += 1; //cont = cont + 1;
+   }while(cont <= 3);
+
+    
+   }
+   }
+
+}
